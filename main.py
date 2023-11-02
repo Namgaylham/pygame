c81488ee631b71986f8dd946c5b4f7648a3dbbd0
Namgaylham <141400802+Namgaylham@users.noreply.github.com>
2023-11-02 20:44:05 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import*
+#from PIL import Image,ImageTK
+
+
+#window screen
+
+root = Tk()
+root.title("rock paper sissor")
+root.configure(background="blue")
+
+root.mainloop()
+
+
+
+ 
