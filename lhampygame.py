cda7cca3eb0c7fed46dabea5b3db51092ffd4541
Namgaylham <141400802+Namgaylham@users.noreply.github.com>
2023-11-03 16:10:50 +0600

    Add files via upload

--- lhampygame.py
+++ lhampygame.py
@@ -1,5 +1,5 @@
 from tkinter import*
-from PIL import Image,ImageTk
+from PIL.Image import Image, ImageTk
 from random import randint
 
 #window display
@@ -29,22 +29,58 @@ computer_score = Label(root,text=0,font=100,bg="blue",fg="black")
 player_score.grid(row=1,column=3)
 computer_score.grid(row=1,column=1)
 
-#button section
-rock = Button(root, width=20,height=2,text="ROCK",
-              bg="pink",fg="black",command= lambda:update_choice("rock")).grid(row=2,column=1)
-paper = Button(root, width=20,height=2,text="PAPER",
-               bg="purple",fg="black",command= lambda:update_choice("paper")).grid(row=2,column=2)
-scissor = Button(root, width=20,height=2,text="SCISSOR",
-                 bg="yellow",fg="black",command= lambda:update_choice("scissor")).grid(row=2,column=3)
 #identy
 user_identy = Label(root,font=50,text="PLAYER",bg="blue")
 computer_identy = Label(root,font=50,text="COMPUTER",bg="blue")
 user_identy.grid(row=0,column=3)
 computer_identy.grid(row=0,column=1)
-=2
 #message
 message = Label(root,font=50,bg="blue",fg="black")
 message.grid(row=3,column=2)
+
+# funtion to update the message and the score
+def update_message(x):
+    message["text"] = x
+#update the user score
+def update_user_score():
+    score = int(player_score["text"])
+    score +=1
+    player_score["text"] = str(score)
+#to update computer score
+def update_computer_score():
+    score = int(computer_score["text"])
+    score +=1
+    computer_score["text"] = str(score)
+
+#to check the winner
+def check_winner(player, computer):
+    if player == computer:
+        update_message("EQUAL!")
+    elif player == "rock":
+        if computer == "paper":
+            update_message("LOSE :(")
+            update_computer_score()
+        else:
+            update_message("VICTORY !!")
+            update_user_score()
+    elif player =="paper":
+         if computer =="scissor":
+             update_message("LOSE:(")
+             update_computer_score()
+         else:
+             update_message("VICTORY !!")
+             update_user_score 
+    elif player == "scissor":
+         if computer=="rock":
+            update_message("LOSE :(")
+            update_computer_score()
+         else:
+             update_message("VICTORY !!")
+             update_user_score()
+    else:
+         pass
+check_winner()
+
 #defining the function to update the choice
 choices= ["rock", "paper", "scissor"]
 def update_choice(x):
@@ -65,51 +101,14 @@ def update_choice(x):
     else:
         user_label.configure(image=scissor_user)
 
-# funtion to update the message and the score
-def update_message(x):
-    message["text"] = x
+#button section
+rock = Button(root, width=20,height=2,text="ROCK",
+              bg="pink",fg="black",command= lambda:update_choice("rock")).grid(row=2,column=1)
+paper = Button(root, width=20,height=2,text="PAPER",
+               bg="purple",fg="black",command= lambda:update_choice("paper")).grid(row=2,column=2)
+scissor = Button(root, width=20,height=2,text="SCISSOR",
+                 bg="yellow",fg="black",command= lambda:update_choice("scissor")).grid(row=2,column=3)
 
-#update the user score
-def update_user_score():
-    score = int(player_score["text"])
-    score +=1
-    player_score["text"] = str(score)
-#to update computer score
-def update_computer_score():
-    score = int(computer_score["text"])
-    score +=1
-    computer_score["text"] = str(score)
-
-
-#to check the winner
-def check_winner(player, computer):
-    if player == computer:
-        update_message("EQUAL!")
-    elif player == "rock":
-        if computer == "paper":
-            update_message("LOSE :(")
-            update_computer_score()
-        else:
-            update_message("VICTORY !!")
-            update_user_score()
-    elif player =="paper":
-         if computer =="scissor":
-             update_message("LOSE:(")
-             update_computer_score()
-        else:
-             update_message("VICTORY !!")
-             update_user_score()
-    elif player == "scissor"
-         if computer=="rock":
-            update_message("LOSE :(")
-            update_computer_score()
-        else:
-             update_message("VICTORY !!")
-             update_user_score()
-    else:
-         pass
-
-check_winner(x,computer_choice)
 
 
 
